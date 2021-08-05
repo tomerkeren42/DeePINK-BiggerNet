8f9b80b643d2af7f912cc91ec6a9f8dc9d784ec7
Tomer Keren <tomerkeren42@gmail.com>
2021-08-05 11:13:37 +0300

    Start work with script attached

---
+++ start_evaluate_deePINK_extension.py
@@ -0,0 +1,10 @@
+# File: start_evaluate_deePINK_extension.py
+# Make sure this exectuble file is placed in the DeePINK-BiggerNet directory.
+
+from quickstart import start_evaluate
+
+# First, install all required packages. Can do it automaticly with uncommenting the next line:
+# pip3 install -r requirements.txt --no-index --find-links file:///tmp/packages
+
+
+start_evaluate()
